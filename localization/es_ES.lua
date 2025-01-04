d5a3cb8b3c00e3be2d35b8fab109e5c844f47921
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-01-04 10:16:54 +0700

    Added placeholder variables

---
+++ localization/es_ES.lua
@@ -0,0 +1,9 @@
+return {
+    misc = {
+        dictionary = {
+            k_voucher_restock_1 = "",
+            k_voucher_restock_2 = "",
+            k_voucher_restock_3 = "",
+        }
+    }
+}
