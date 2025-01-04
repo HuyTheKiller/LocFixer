af590a40ceddd8fa80db743956e91d4af88a654d
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-01-04 09:48:59 +0700

    Added main file

---
+++ LocFixer.lua
@@ -0,0 +1,22 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: LocFixer
+--- MOD_ID: LoFi
+--- MOD_AUTHOR: [HuyTheKiller, Balatro Discord]
+--- MOD_DESCRIPTION: Addresses all localization issues in vanilla game, ensuring all languages get everywhere.
+--- PRIORITY: 9999999999
+--- CONFLICTS: [Vietnamese Balatro]
+--- DISPLAY_NAME: LocFixer
+--- VERSION: 1.0.0
+
+----------------------------------------------
+------------MOD CODE -------------------------
+if SMODS.Atlas then
+	SMODS.Atlas({
+	  key = "modicon",
+	  path = "icon.png",
+	  px = 32,
+	  py = 32
+	})
+  end
+----------------------------------------------
+------------MOD CODE END----------------------
