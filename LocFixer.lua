fc15214d8320dbf0394518189ae804b8ae3709d8
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-02-19 23:22:00 +0700

    Removed legacy header

--- LocFixer.lua
+++ LocFixer.lua
@@ -1,21 +1,7 @@
---- STEAMODDED HEADER
---- MOD_NAME: LocFixer
---- MOD_ID: LoFi
---- MOD_AUTHOR: [HuyTheKiller, Balatro Discord]
---- MOD_DESCRIPTION: Addresses all localization issues in vanilla game, ensuring all languages get everywhere.
---- PRIORITY: 9999999999
---- DISPLAY_NAME: LocFixer
---- VERSION: 1.1.0
-
-----------------------------------------------
-------------MOD CODE -------------------------
-if SMODS.Atlas then
-	SMODS.Atlas({
-	  key = "modicon",
-	  path = "icon.png",
-	  px = 32,
-	  py = 32
-	})
-  end
-----------------------------------------------
-------------MOD CODE END----------------------
+-- Pure lovely mod with a fancy mod icon, for now
+SMODS.Atlas({
+	key = "modicon",
+	path = "icon.png",
+	px = 32,
+	py = 32
+})
