8896d38e2b43ec7847f86c4dedb4aa86ac86be20
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-02-20 18:27:42 +0700

    Improved latam Spanish

--- localization/es_419.lua
+++ localization/es_419.lua
@@ -4,7 +4,7 @@ return {
             k_voucher_restock_1 = "DERROTA LA",
             k_voucher_restock_2 = "CIEGA JEFE",
             k_voucher_restock_3 = "PARA RENOVARSE",
-            k_cry_voucher_restock_2 = "DOS CIEGA JEFES", -- confirm this for me
+            k_cry_voucher_restock_2 = "DOS CIEGAS DE JEFE",
         }
     }
 }
