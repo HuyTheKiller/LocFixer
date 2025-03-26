683596cb686266d53046130b693b90ef8c444fed
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-03-26 15:53:37 +0700

    Added Simplified Chinese

--- localization/zh_CN.lua
+++ localization/zh_CN.lua
@@ -1,10 +1,10 @@
 return {
     misc = {
         dictionary = {
-            k_voucher_restock_1 = "",
-            k_voucher_restock_2 = "",
-            k_voucher_restock_3 = "",
-            k_cry_voucher_restock_2 = "",
+            k_voucher_restock_1 = "击败",
+            k_voucher_restock_2 = "Boss盲注",
+            k_voucher_restock_3 = "以补货",
+            k_cry_voucher_restock_2 = "个Boss盲注",
         }
     }
 }
