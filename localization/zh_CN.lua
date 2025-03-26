8490d7e6f70bb9a1e615cfb2ad8ea6481863f77e
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-03-26 20:53:42 +0700

    Oops, missing "两" (2)

--- localization/zh_CN.lua
+++ localization/zh_CN.lua
@@ -4,7 +4,7 @@ return {
             k_voucher_restock_1 = "击败",
             k_voucher_restock_2 = "Boss盲注",
             k_voucher_restock_3 = "以补货",
-            k_cry_voucher_restock_2 = "个Boss盲注",
+            k_cry_voucher_restock_2 = "两个Boss盲注",
         }
     }
 }
