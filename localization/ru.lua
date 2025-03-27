bb19661cc52ce4ea897c164aa997a61ff3d2c401
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-03-27 17:49:56 +0700

    Update ru.lua
    Prompts from said Improved Russian mod.

--- localization/ru.lua
+++ localization/ru.lua
@@ -1,9 +1,9 @@
 return {
     misc = {
         dictionary = {
-            k_voucher_restock_1 = "",
-            k_voucher_restock_2 = "",
-            k_voucher_restock_3 = "",
+            k_voucher_restock_1 = "ПОБЕДИТЕ",
+            k_voucher_restock_2 = "БОСС-БЛАЙНД",
+            k_voucher_restock_3 = "ДЛЯ ПОПОЛНЕНИЯ",
             k_cry_voucher_restock_2 = "",
             k_peek_deck_1 = "",
             k_peek_deck_2 = "",
