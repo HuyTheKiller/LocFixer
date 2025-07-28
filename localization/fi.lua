5af006586ed15bfa93fdd324eb15b2f94341a489
Kim Takala <144361787+kimtakala@users.noreply.github.com>
2025-07-28 06:53:01 +0300

    Update fi.lua

--- localization/fi.lua
+++ localization/fi.lua
@@ -4,7 +4,7 @@ return {
             k_voucher_restock_1 = "VOITA",
             k_voucher_restock_2 = "POMOBLINDI",
             k_voucher_restock_3 = "LISÄTÄKSESI",
-            k_cry_voucher_restock_2 = "KAKSI POMO BLINDIA",
+            k_cry_voucher_restock_2 = "KAKSI POMOBLINDIA",
             k_peek_deck_1 = "KURKISTA",
             k_peek_deck_2 = "PAKKAAN",
         }
